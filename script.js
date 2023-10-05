4f55adfdbddc44a73fcdfaa532b253030bfbb1c5
JuliaPasquier <pasquierjulia5@gmail.com>
2023-10-05 14:34:46 +0200

    Correction 1 eventListener

--- script.js
+++ script.js
@@ -3,32 +3,37 @@
 let scoreJ1 = 0;
 let scoreJ2 = 0
 let scoreTotal = 0
+
+//Mes constantes
 const scoreMax = 5
+const J1 = document.getElementById("scoreJ1");
+const J2 = document.getElementById("scoreJ2");
+const boutonJ1 = document.getElementById("boutonJ1");
+const boutonJ2 = document.getElementById("boutonJ2");
 
 
-// Mes EventListener
-document.getElementById("boutonJ1").addEventListener("click", function () {
-    scoreJ1++
-    updateScore();
-    checkWinner();
-})
-
-document.getElementById("boutonJ2").addEventListener("click", function () {
-    scoreJ2++
-    updateScore();
-    checkWinner();
-})
-
-document.getElementById("reset").addEventListener("click",function(){
-    resetScore();
-    
+//Mon Event Listener
+document.addEventListener("click", (e) => {
+    if (e.target.matches("#boutonJ1")) {
+        scoreJ1++
+        updateScore();
+        checkWinner();
+    }
+    if (e.target.matches("#boutonJ2")) {
+        scoreJ2++
+        updateScore();
+        checkWinner();
+    }
+    if (e.target.matches("#reset")) {
+        resetScore();
+    }
 })
 
 
 //Mes functions
-function updateScore(){
-    document.getElementById("scoreJ1").textContent = scoreJ1
-    document.getElementById("scoreJ2").textContent = scoreJ2
+function updateScore() {
+    J1.textContent = scoreJ1
+    J2.textContent = scoreJ2
 }
 
 function checkWinner() {
@@ -39,27 +44,27 @@ function checkWinner() {
     }
 }
 
-function attribuerCouleur(){
-    if(scoreJ1 > scoreJ2){
-        document.getElementById("scoreJ1").style.color = "green"
-        document.getElementById("scoreJ2").style.color = "red"
-    }else if(scoreJ1 < scoreJ2) {
-        document.getElementById("scoreJ1").style.color = "red"
-        document.getElementById("scoreJ2").style.color = "green"
-    }else{
-        document.getElementById("scoreJ1").style.color = "black"
-        document.getElementById("scoreJ2").style.color = "black"
+function attribuerCouleur() {
+    if (scoreJ1 > scoreJ2) {
+        J1.style.color = "green"
+        J2.style.color = "red"
+    } else if (scoreJ1 < scoreJ2) {
+        J1.style.color = "red"
+        J2.style.color = "green"
+    } else {
+        J1.style.color = "black"
+        J2.style.color = "black"
     }
 }
 
-function freezeScore(){
-    document.getElementById("boutonJ1").disabled = true;
-    document.getElementById("boutonJ2").disabled = true;
+function freezeScore() {
+    boutonJ1.disabled = true;
+    boutonJ2.disabled = true;
 }
 
-function resetScore(){
-    document.getElementById("boutonJ1").disabled = false;
-    document.getElementById("boutonJ2").disabled = false;
+function resetScore() {
+    boutonJ1.disabled = false;
+    boutonJ2.disabled = false;
     scoreJ1 = 0
     scoreJ2 = 0
     scoreTotal = 0
